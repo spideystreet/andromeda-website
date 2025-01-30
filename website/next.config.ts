8dd1859aac70deee3e6beefec58b1421ab5d72a0
spideystreet <dhicham.pro@gmail.com>
2025-01-30 21:31:22 +0100

    fix(eslint)

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  trailingSlash: false,
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
